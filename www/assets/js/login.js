ffea6fb1c847fe496bf57092da7f704e08a91b96
lxyxynt <lxyxynt@gmail.com>
2013-05-22 16:13:19 +0800

    add some easy logic code about Role

--- www/assets/js/login.js
+++ www/assets/js/login.js
@@ -8,9 +8,24 @@
         getView($view);
     }); 
 
+    function getRole(username, password) {
+        if (username == "lxyxynt" && password == "123456")
+            return "student";
+        if (username == "admin" && password == "admin")
+            return "admin";
+        if (username == "student" && password == "student")
+            return "student";
+        if (username == "professor" && password == "professor")
+            return "professor";
+        return null;
+    }
+
     var getView = function ($view) {
         $view.children('form').submit(function () {
-            data.role = $('.username input').val();
+            //data.role = $('.username input').val();
+            data.role = getRole($('.username input').val(), 
+                $('.password input').val());
+            
             console.log(data.role);
             window.location.hash = "#dashboard";
             return false;
